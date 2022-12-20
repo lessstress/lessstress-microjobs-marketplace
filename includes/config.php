2c6071f4b6f3cf133d7f5cedd42980b78e23df6b
Clifford Ettah <sircliffjoe11@gmail.com>
2022-12-20 12:45:49 +0100

    initial commit

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+$config['db']['host'] = 'localhost';
+$config['db']['name'] = 'lessstress';
+$config['db']['user'] = 'root';
+$config['db']['pass'] = '';
+$config['db']['pre'] = 'ql_';
+
+$config['admin_folder'] = 'admin';
+$config['version'] = '1.0';
+$config['installed'] = '1';
+?>
